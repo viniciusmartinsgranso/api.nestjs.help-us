05ed6d2bea6f57736eb7eed3f1c0ff1c2936e533
unknown <manut@C-264.labinfo.facens.br>
2024-07-14 02:07:35 -0300

    remove useless functions

--- src/modules/occurrences/services/occurrences.service.ts
+++ src/modules/occurrences/services/occurrences.service.ts
@@ -83,28 +83,4 @@ export class OccurrencesService {
       userId: requestUser.id,
     });
   }
-
-  private calculateDistance(
-    lat1: number,
-    lon1: number,
-    lat2: number,
-    lon2: number,
-  ): number {
-    const R = 6371; // Raio da Terra em km
-    const dLat = this.deg2rad(lat2 - lat1);
-    const dLon = this.deg2rad(lon2 - lon1);
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(this.deg2rad(lat1)) *
-        Math.cos(this.deg2rad(lat2)) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    // Distância em km
-    return R * c;
-  }
-
-  private deg2rad(deg: number): number {
-    return deg * (Math.PI / 180);
-  }
 }
