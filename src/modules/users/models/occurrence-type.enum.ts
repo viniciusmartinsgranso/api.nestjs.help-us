3bbbb27ecaa71278c3855c8d98d96db79a749974
Vinícius Martins Granso <viniciusmartinsg@hotmail.com>
2024-10-20 02:08:09 -0300

    feat(occurrences): add new occurrence types

--- src/modules/users/models/occurrence-type.enum.ts
+++ src/modules/users/models/occurrence-type.enum.ts
@@ -2,12 +2,14 @@ export enum OccurrenceTypeEnum {
   CRASH = 0,
   FIRE = 1,
   CAVE_IN = 2,
-  COOP = 3,
+  WINDS = 3,
+  SHOOTING = 4
 }
 
 export const occurrenceTypeTranslate: Record<OccurrenceTypeEnum, string> = {
   [OccurrenceTypeEnum.CRASH]: 'Batida',
   [OccurrenceTypeEnum.FIRE]: 'Incendio',
   [OccurrenceTypeEnum.CAVE_IN]: 'Desmoronamento',
-  [OccurrenceTypeEnum.COOP]: 'Policia',
+  [OccurrenceTypeEnum.WINDS]: 'Vento forte',
+  [OccurrenceTypeEnum.SHOOTING]: 'Tiroteio',
 };
